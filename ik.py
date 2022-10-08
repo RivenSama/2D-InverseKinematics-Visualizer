35b4096c8757c84a17f704230046fb78dbac573b
RivenSama <ervinpapp07@gmail.com>
2022-10-08 09:04:38 +0100

    Test

--- ik.py
+++ ik.py
@@ -6,45 +6,43 @@ from Arm import Arm
 from ArmLinker import ArmLinker
 
 armLinker = ArmLinker(0,0)
-armLinker.AddArm(10)
-armLinker.AddArm(5)
-armLinker.AddArm(2)
+x = 50
+y = 50
 
+def AddArmToLinker(x):
+    armLinker.AddArm(10)
 
-def onclick(event):
-        
-
-
-    plt.cla()
-
-    plt.axis([-20, 20, -20, 20])
-    plt.grid(['both'])
-    
-    
-
-    armLinker.Reach(event.xdata,event.ydata)
+def DrawArm():
     for i in armLinker.armSeg:
-        
         plt.scatter([i.x, i.GetEndX()],[i.y,i.GetEndY()])
         plt.plot([i.x, i.GetEndX()],[i.y,i.GetEndY()])
 
+
+
+def onclick(event):
+    armLinker.Reach(event.xdata,event.ydata)
+    DrawCanvas()
+    DrawArm()
     plt.draw()
+    
+    
 
 
 
+def DrawCanvas():
+    plt.cla()
+    plt.axis([-x, x, -y, y])
+    plt.grid(['both'])
+    
     
 
 
 def CreateFig():
     
     fig, ax = plt.subplots()
-    ax.axis([-20, 20, -20, 20])
-    ax.grid(['both'])
-
-    #cid = fig.canvas.mpl_connect('motion_notify_event', on_move)
+    DrawCanvas()
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
-
-
+    DrawArm()
     plt.show()
 
  
@@ -53,5 +51,8 @@ def CreateFig():
 
 
 if __name__ == '__main__':
+    AddArmToLinker(5)
+    AddArmToLinker(5)
+    AddArmToLinker(5)
     CreateFig()
     
